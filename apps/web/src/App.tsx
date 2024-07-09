607d8f95ba4907dfb352ef3568beb9624ecb7fce
unknown <vimalthanth70@gmail.com>
2024-07-09 18:21:05 +0530

    fixed web build

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
